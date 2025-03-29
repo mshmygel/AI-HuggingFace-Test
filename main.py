cce72a49662845b65c9521536f372d86597a8a43
Mike Shmygel <m.shmygel@gmail.com>
2025-03-29 13:21:17 +0200

    add and test facebook/bart-large-cnn model

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from transformers import pipeline
 
+import torch
+
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
+
 model = pipeline("summarization", model="facebook/bart-large-cnn")
 response = model("text to summarize")
 print(response)
